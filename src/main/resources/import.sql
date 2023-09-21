47b9cee3273e188a9f7a20b7d3874037ccb21e7a
MariaMuniz <cidamuniz2011@yahoo.com.br>
2023-09-21 18:11:20 -0300

    BycrypPassword

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$J7PvVZVPh6Rgg0I8slwzTuNPs1mYjEjHkJJuINuc4PYJkm/tzNcKW');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$J7PvVZVPh6Rgg0I8slwzTuNPs1mYjEjHkJJuINuc4PYJkm/tzNcKW');
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
